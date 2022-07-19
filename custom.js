b07dca8858729e77b802065f897f8463d0991fe3
Dave Price <d.price@elifesciences.org>
2022-07-19 14:59:25 +0100

    Add new content and javascript for second column

--- custom.js
+++ custom.js
@@ -1,9 +1,16 @@
+const exampleFour = document.querySelector('.example-four');
+
 const tabOne = document.querySelector('.tab-one');
 const tabTwo = document.querySelector('.tab-two');
 const tabThree = document.querySelector('.tab-three');
+
 const contentOne = document.querySelector('.content__one');
 const contentTwo = document.querySelector('.content__two');
 const contentThree = document.querySelector('.content__three');
+
+const articleStatus = document.querySelector('.article-status');
+const reviewTimeline = document.querySelector('.review-timeline');
+const articleMetadata = document.querySelector('.article-metadata');
 contentOne.classList.add('show');
 
 tabOne.addEventListener('click', function(event) {
@@ -36,5 +43,30 @@ tabThree.addEventListener('click', function(event) {
   contentOne.classList.remove('show');
   contentTwo.classList.remove('show');
   contentThree.classList.add('show');
-  console.log('yum3');
+
 })
+
+function secondaryColumnCollapse() {
+  const evaluationPosition = document.querySelector('.evaluation-summary__header');
+  let bounding = evaluationPosition.getBoundingClientRect();
+
+  if (bounding.top <= 0 ) {
+    articleStatus.classList.add('hide');
+    articleMetadata.classList.add('hide');
+    reviewTimeline.classList.add('hide');
+    console.log('yeehawwww');
+  } else {
+    articleStatus.classList.remove('hide');
+    articleMetadata.classList.remove('hide');
+    reviewTimeline.classList.remove('hide');
+  };
+
+}
+
+function detectColumnPosition() {
+  // possibly long task
+  setInterval(detectColumnPosition, 500);
+  secondaryColumnCollapse();
+}
+
+detectColumnPosition();
