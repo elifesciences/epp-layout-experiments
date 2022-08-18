8a8169562e5871842f3cb8a37f37659dddbffdfc
Dave Price <d.price@elifesciences.org>
2022-08-18 16:14:02 +0100

    add js functionality for examples 3/4 tabs

--- custom2.js
+++ custom2.js
@@ -12,34 +12,56 @@ contentOne.classList.add('show');
 
 tabOne.addEventListener('click', function(event) {
   event.preventDefault();
+  tabOneFunction();
+})
+
+tabTwo.addEventListener('click', function(event) {
+  event.preventDefault();
+  tabTwoFunction();
+})
+
+tabThree.addEventListener('click', function(event) {
+  event.preventDefault();
+  tabThreeFunction();
+})
+
+function tabOneFunction() {
   tabTwo.classList.remove('active');
   tabThree.classList.remove('active');
   tabOne.classList.add('active');
   contentOne.classList.add('show');
   contentTwo.classList.remove('show');
   contentThree.classList.remove('show');
-  console.log('yum');
-});
+}
 
-tabTwo.addEventListener('click', function(event) {
-  event.preventDefault();
+function tabTwoFunction() {
   tabOne.classList.remove('active');
   tabThree.classList.remove('active');
   tabTwo.classList.add('active');
   contentOne.classList.remove('show');
   contentTwo.classList.add('show');
   contentThree.classList.remove('show');
-  console.log('yum2');
-});
+}
 
-tabThree.addEventListener('click', function(event) {
-  event.preventDefault();
+function tabThreeFunction() {
   tabOne.classList.remove('active');
   tabTwo.classList.remove('active');
   tabThree.classList.add('active');
   contentOne.classList.remove('show');
   contentTwo.classList.remove('show');
   contentThree.classList.add('show');
+}
+
+const authorResponse = document.querySelector('.author-response-js');
+authorResponse.addEventListener('click', function(event) {
+  console.log('test');
+  tabThreeFunction();
+});
+
+const reviews = document.querySelector('.review-js');
+reviews.addEventListener('click', function(event) {
+  console.log('test2');
+  tabThreeFunction();
 });
 
 document.addEventListener('scroll', function() {
